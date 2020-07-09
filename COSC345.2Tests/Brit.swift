1d4c5a5c9dea045f61262371445b6111ff439e0f
Brittany Duncan <brittanyduncan@brittanys-air.student.uod.otago.ac.nz>
2020-07-10 09:25:21 +1200

    checking continuous integration

--- COSC345.2Tests/Brit.swift
+++ COSC345.2Tests/Brit.swift
@@ -7,35 +7,36 @@
 //
 
 
-import SpriteKit
+//import SpriteKit
+//
+//class Brit: SKScene {
+//
+//    var label = SKLabelNode(fontNamed: "ArialMT")
+//
+//    var score: Int = 0 {
+//        didSet {
+//            label.text = "Score: \(score)"
+//        }
+//    }
+//
+//    override func didMove(view: SKView) {
+//
+//
+//        label.fontSize = 24
+//        label.text = "Score: \(score)"
+//        
+//        //Place the label in upper left corner of the screen
+//        label.position = CGPoint(x: self.frame.width / 2 + 60, y: self.frame.height / 2 + 335)
+//
+//        //Add this
+//        label.horizontalAlignmentMode = SKLabelHorizontalAlignmentMode.left
+//
+//        addChild(label)
+//    }
+//
+//    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+//
+//        score+=5
+//    }
+//}
 
-class Brit: SKScene {
-    
-    var label = SKLabelNode(fontNamed: "ArialMT")
-    
-    var score: Int = 0 {
-        didSet {
-            label.text = "Score: \(score)"
-        }
-    }
-    
-    override func didMove(view: SKView) {
-        
-        
-        label.fontSize = 24
-        label.text = "Score: \(score)"
-        
-        //Place the label in upper left corner of the screen
-        label.position = CGPoint(x: self.frame.width / 2 + 60, y: self.frame.height / 2 + 335)
-        
-        //Add this
-        label.horizontalAlignmentMode = SKLabelHorizontalAlignmentMode.left
-        
-        addChild(label)
-    }
-    
-    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        
-        score+=5
-    }
-}
